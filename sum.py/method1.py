cc668d78db98a00cf225028556d84f4a21574b92
arshleen18 <84401040+arshleen18@users.noreply.github.com>
2021-05-24 03:27:12 +0530

    Create method1.py
    a program to find the sum of first n natural numbers

---
+++ sum.py/method1.py
@@ -0,0 +1,9 @@
+#sum of first n natural numbers
+
+num = int(input('enter that number till which you want the sum: '))
+sum = 0
+i= 1
+while (i <= num):
+    sum += i
+    i += 1
+print('sum: ', sum)

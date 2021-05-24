61fc0d6b694440533f3284f7d3a88c6752358e9f
arshleen18 <84401040+arshleen18@users.noreply.github.com>
2021-05-24 19:28:51 +0530

    Create method1.py
    programs to print various star pattern

---
+++ star pattern.py/method1.py
@@ -0,0 +1,12 @@
+#wap to print following star pattern:
+*
+***
+******
+**********
+***************
+
+n = int(input('enter number: '))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print('*'* j, end='')
+    print()
